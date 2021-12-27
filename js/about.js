45ead6c7c9a4d1920b42900ad31b4f17e8ee901f
Juan Diego Gómez <juandieruiz6@gmail.com>
2021-12-27 13:22:42 +0100

    localStorage theme

--- js/about.js
+++ js/about.js
@@ -1,17 +1,30 @@
 // JavaScript Document
 $(document).ready(function () {
+
   // Selector de tema
   let theme = $("#theme");
 
   $("#light-mode").click(function () {
     theme.attr("href", "css/lightmode.css");
+    localStorage.setItem("theme", "light-mode");
   });
   $("#dark-mode").click(function () {
     theme.attr("href", "css/darkmode.css");
+    localStorage.setItem("theme", "dark-mode");
   });
   $("#hacker-mode").click(function () {
     theme.attr("href", "css/hackermode.css");
+    localStorage.setItem("theme", "hacker-mode");
   });
+  localStorage.getItem("theme");
+  if (localStorage.getItem("theme") == "light-mode") {
+    theme.attr("href", "css/lightmode.css");
+  } else if (localStorage.getItem("theme") == "dark-mode") {
+    theme.attr("href", "css/darkmode.css");
+  } else if (localStorage.getItem("theme") == "hacker-mode") {
+    theme.attr("href", "css/hackermode.css");
+  }
+
 
   // Scroll arriba
   $(".subir").click(function () {
